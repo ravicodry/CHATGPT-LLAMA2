1a99cf39a5884f07d592b01b1f62fc1a5ca974ce
Ravi Choudhary <ravichoudhary141991@gmail.com>
2023-10-08 11:01:55 +0530

    Add files via upload

--- new1.py
+++ new1.py
@@ -4,9 +4,23 @@ import torch
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import subprocess
 torch.cuda.empty_cache()
 #from transformers import set_token
 import streamlit as st
+
+api_token = st.text_input("Enter your Hugging Face API Token:", type="password")
+
+# Create a button to submit the token and run Hugging Face CLI
+if st.button("Run Hugging Face CLI"):
+    # Check if the user entered a token
+    if api_token:
+        # Set the HUGGINGFACE_TOKEN environment variable with the provided token
+        # This allows the Hugging Face CLI to authenticate API requests
+        subprocess.run(["transformers-cli", "login", f"--api_key {api_token}"])
+        st.success("Hugging Face CLI is authenticated with the provided API Token.")
+    else:
+        st.warning("Please enter an API Token.")
 st.title("Chatbot App")
 user_input = st.text_input("")
 # Set your Hugging Face API token
