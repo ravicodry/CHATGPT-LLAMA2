001f5b8291e23a433f63d5f18fe6e3bf743b88d6
Ravi Choudhary <ravichoudhary141991@gmail.com>
2023-10-08 13:36:09 +0530

    Add files via upload

--- new1.py
+++ new1.py
@@ -21,30 +21,37 @@ import streamlit as st
 #         else:
 #             st.success('Proceed to entering your prompt message!', icon='👉')
 #     os.environ['HUGGINGFACEHUB_API_TOKEN'] = HUGGINGFACEHUB_API_TOKEN
-st.title("Chatbot App")
-user_input = st.text_input("")
-# Set your Hugging Face API token
-#set_token("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")
-conversation = []
-model = "meta-llama/Llama-2-7b-chat-hf"
+def main():
+  
+    st.title("Chatbot App")
+    user_input = st.text_input("")
+    # Set your Hugging Face API token
+    #set_token("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")
+    conversation = []
+    model = "meta-llama/Llama-2-7b-chat-hf"
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
-if user_input:
-  conversation.append(f"{user_input}")
-  sequences = pipeline(
-      " ".join(conversation),
-      do_sample=True,
-      top_k=10,
-      num_return_sequences=1,
-      eos_token_id=tokenizer.eos_token_id,
-      max_length=50,
-  )
-  # for seq in sequences:
-  #     print(f"Result: {seq['generated_text']}")
-  st.write(sequences)
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+    if user_input:
+
+        conversation.append(f"{user_input}")
+        sequences = pipeline(
+            " ".join(conversation),
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+            max_length=50,
+    )
+    # for seq in sequences:
+    #     print(f"Result: {seq['generated_text']}")
+    st.write(sequences)
+
+if __name__ == "__main__":
+
+  main()
